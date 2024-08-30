dd313d38e92729843625a588065a54a9a0206d9a
mnomnanmnasir <nomannasir@i8is.com>
2024-08-30 11:27:13 +0500

    Add files via upload

--- src/comp/SideBar/SideBar.js
+++ src/comp/SideBar/SideBar.js
@@ -29,7 +29,7 @@ const SideBar = ({ items, setItems, icons }) => {
                         className={`sidebar-item ${item.isActive ? 'selected' : ''}`}
                         onClick={() => {
                             if (item.path === "/") {
-                                window.location.href = "https://infiniti-suite-final.vercel.app/";
+                                window.location.href = "https://infiniti-suite-web.vercel.app/header";
                             } else {
                                 navigate(item.path);
                                 setItems((prevItems) => prevItems.map((it, ind) => index === ind ? { ...it, isActive: true } : { ...it, isActive: false }));
